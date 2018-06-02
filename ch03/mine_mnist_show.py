da051ec691cc640e9f057a8c22d07b18c240b05a
takanobu0210 <takanobu.030210@gmail.com>
2018-06-02 13:55:27 +0900

    ゼロから作るDeepLearning ch3

---
+++ ch03/mine_mnist_show.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+import numpy as np
+from dataset.mnist import load_mnist
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+# minstのデータを読み込む
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+img = x_train[0]
+label = t_train[0]
+
+# 0番目のデータを表示してみる
+print(label) # 5
+print(img.shape) # (784,)
+img = img.reshape(28, 28) # 形状を元の画像サイズに変形
+print(img.shape)
+
+img_show(img)
